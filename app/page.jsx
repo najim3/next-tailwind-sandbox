91cfc6b4ceaf3186fd5bb3e43670ceccbafd9335
Najim Uddin <engr.najim3@gmail.com>
2023-06-28 09:17:07 +0600

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis,
+        cum. Officia quia, obcaecati atque quibusdam unde provident id?
+        Consectetur, corporis dolores quidem a asperiores porro, fugiat ab
+        repudiandae tempore natus, reiciendis saepe impedit soluta! Praesentium
+        perferendis dolor consectetur iure, tenetur similique vel! Voluptas
+        aspernatur recusandae nobis, deserunt minima earum obcaecati.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
